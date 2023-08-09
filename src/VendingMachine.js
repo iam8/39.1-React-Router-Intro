328a2a5f8fd1a12ba97061087e85e5b6a1f0a0a5
alexm <alex-m@outlook.com>
2023-08-08 17:01:26 -0700

    Add Link component for each snack

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,11 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 function VendingMachine () {
 
     return (
         <div className="VendingMachine">
-            This is the Vending Machine. Snack choices: Twiglets, Cookies, or Bubbly Water.
+            <h1>Vending Machine</h1>
+            <div>
+                Click on a snack below for more information:
+                <ul>
+                    <li>
+                        <Link to="/twiglets">
+                            Twiglets
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link to="/cookies">
+                            Cookies
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link to="/bubbly-water">
+                            Bubbly Water
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
